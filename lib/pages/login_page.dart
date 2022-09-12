8f940a9630ccb5660e874cce05ed882fcbd91b69
kelvins213 <kelvinotilio@gmail.com>
2022-09-12 13:12:20 -0300

    vaildaçao de email e senha implementados

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import  'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:netflix_clone/data/netflix_datas.dart';
 import 'package:netflix_clone/domain/netflix_atributes.dart';
+import 'package:netflix_clone/pages/home_page.dart';
 
 class LoginPage extends StatefulWidget{
   const LoginPage({Key? key}) : super(key: key);
@@ -12,49 +15,112 @@ class LoginPage extends StatefulWidget{
 
 class _LoginPage extends State<LoginPage>{
   NetflixLogin objeto = NetflixDatabase.login;
+
+  TextEditingController userController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(36.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.network(objeto.logo),
-              buildTextFormField(text: "Your Email"),
-              buildTextFormField(text: "Your Password"),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFD2001A)),
+      body: ListView(
+        children: [
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Image.network(objeto.logo),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const SizedBox(height: 48),
+                          TextFormField(
+                            controller: userController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Campo de e-mail obrigatório';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Color(0xFFE4DCCF),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              labelText: "Your e-mail",
+                              floatingLabelStyle: TextStyle(color: Colors.grey, fontSize: 18),
+                            ),
+                          ),
+                          const SizedBox(height: 48),
+                          TextFormField(
+                            controller: passwordController,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Campo de senha obrigatório';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Color(0xFFE4DCCF),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              labelText: "Your password",
+                              floatingLabelStyle: TextStyle(color: Colors.grey, fontSize: 18),
+                            ),
+                          ),
+                          const SizedBox(height: 32),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFD2001A)
+                      ),
+                      onPressed: onPressed,
+                      child: const Text("Login", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                    ),
+                  ],
                 ),
-                onPressed: onPressed,
-                child: const Text("Login", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
               ),
-            ],
-          ),
-        ),
+            ),
+        ],
       ),
     );
   }
+  void onPressed() {
+    if (_formKey.currentState!.validate()) {
 
-  buildTextFormField({
-    required String text,
-  }){
-    return TextField(
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: Colors.grey,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
+      String userLogin = "joao@gmail.com";
+      String passwordLogin = "123456";
+
+      String user = userController.text;
+      String pwd = passwordController.text;
+
+      if (userLogin == user && passwordLogin == pwd) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const HomePage();
+            },
           ),
-          labelText: text,
-        ),
-      );
+        );
+      } else {
+        print("Usuario/Senha incorretos");
+      }
+    } else {
+      print("Formulário invalido");
+    }
   }
-
-  onPressed(){}
 
 }
