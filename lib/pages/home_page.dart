edadd9c45a1b059917a2b9f98c844ca0751e2a21
Kelvin <103438311+kelvins213@users.noreply.github.com>
2022-09-12 14:41:47 -0300

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/data/netflix_datas.dart';
+import 'package:netflix_clone/domain/netflix_atributes.dart';
 
 class HomePage extends StatefulWidget{
   const HomePage({Key? key}) : super(key: key);
@@ -8,8 +10,54 @@ class HomePage extends StatefulWidget{
 
 class _HomePage extends State<HomePage>{
 
+  String logo = NetflixDatabase.login.logo;
+
   @override
   Widget build(BuildContext context){
-    return const Text("still building");
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Image.network(
+                logo,
+                height: 40,
+              ),
+              creatElevatedButton(text: 'Séries'),
+              creatElevatedButton(text: 'Filmes'),
+              creatElevatedButton(text: 'Minha lista'),
+            ],
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+
+        ],
+      ),
+    );
   }
+  creatElevatedButton({
+    required String text,
+  }){
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.black,
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  onPressed(){}
+
 }
