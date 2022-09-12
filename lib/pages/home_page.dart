984405840c35bef5ce54113b8d67d45cd5c93db7
kelvins213 <kelvinotilio@gmail.com>
2022-09-12 16:58:16 -0300

    scroll deu pau

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,6 +15,7 @@ class _HomePage extends State<HomePage>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
         centerTitle: true,
@@ -33,10 +34,22 @@ class _HomePage extends State<HomePage>{
           ),
         ),
       ),
-      body: ListView(
-        children: [
-
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Column(
+              children: [
+                writeText(text: 'Populares na Netflix'),
+                createScrollView(),
+                writeText(text: 'Em alta'),
+                createScrollView(),
+                writeText(text: 'Assista de novo'),
+                createScrollView(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -58,6 +71,47 @@ class _HomePage extends State<HomePage>{
     );
   }
 
-  onPressed(){}
+  writeText({
+    required String text,
+  }){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(text, style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+      ],
+    );
+  }
 
+  createScrollView() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          createColumn(),
+        ],
+      ),
+    );
+  }
+
+  createColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.network('https://static.stealthelook.com.br/wp-content/uploads/2021/11/o-ranking-das-series-mais-assistidas-da-netflix-e-porque-voce-deveria-assisti-las-sweet-tooth-20211108204454.jpg', height: 160),
+            Image.network('https://static.stealthelook.com.br/wp-content/uploads/2021/11/o-ranking-das-series-mais-assistidas-da-netflix-e-porque-voce-deveria-assisti-las-sweet-tooth-20211108204454.jpg', height: 160),
+            Image.network('https://static.stealthelook.com.br/wp-content/uploads/2021/11/o-ranking-das-series-mais-assistidas-da-netflix-e-porque-voce-deveria-assisti-las-sweet-tooth-20211108204454.jpg', height: 160),
+            Image.network('https://static.stealthelook.com.br/wp-content/uploads/2021/11/o-ranking-das-series-mais-assistidas-da-netflix-e-porque-voce-deveria-assisti-las-sweet-tooth-20211108204454.jpg', height: 160),
+            Image.network('https://static.stealthelook.com.br/wp-content/uploads/2021/11/o-ranking-das-series-mais-assistidas-da-netflix-e-porque-voce-deveria-assisti-las-sweet-tooth-20211108204454.jpg', height: 160),
+          ],
+        ),
+      ],
+    );
+  }
+  onPressed(){}
 }
